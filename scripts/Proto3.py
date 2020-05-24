9d83ab52a23f3d2bbe89f17ff43420ac26ffa93b
David Parmenter <dparment@adobe.com>
2020-05-24 07:32:53 -0400

    almost works

--- scripts/Proto3.py
+++ scripts/Proto3.py
@@ -1,69 +1,205 @@
 import enum
 import queue
 from transitions import Machine
+import random
 
-TODAY = 0
+TODAY = 1
 
 class Card:
-    def __init__(self, section, id, repetitions=0, interval=1, easiness=2.5, next_practice=0):
+    def __init__(self, section, id, time, repetitions=0, interval=1, easiness=2.5, next_practice=0):
         self.section = section
         self.id = id
+        self.time = time
         self.repetitions = repetitions
         self.interval = interval
         self.easiness = easiness
         self.next_practice = next_practice
 
+    def update(self, quality, time):
+        # SM-2
+        assert quality >= 0 and quality <= 5
+        self.easiness = max(1.3, self.easiness + 0.1 - (5.0 - quality) * (0.08 + (5.0 - quality) * 0.02))
+        if quality < 3:
+            self.repetitions = 0
+        else:
+            self.repetitions += 1
+        if self.repetitions == 1:
+            self.interval = 1
+        elif self.repetitions == 2:
+            self.interval = 6
+        else:
+            self.interval *= self.easiness
+        self.time = time
+
+
+    def __repr__(self):
+        return('{section} / {id}  | [time:{time}] [rep:{repetitions}] [int:{interval}] [ease:{easiness}] [next:{next_practice}]'.format(
+            section=self.section, id = self.id, time=self.time, repetitions=self.repetitions, interval=self.interval,
+            easiness=self.easiness, next_practice=self.next_practice
+        ))
+
 
 CARD_DECK = [
-    Card('section 1', 1),
-    Card('section 1', 2),
-    Card('section 2', 3),
-    Card('section 3', 4),
-    Card('section 4', 5),
-    Card('section 4', 6),
-    Card('section 4', 7),
+    Card('AAA', 1, 0),
+    Card('AAA', 2, 0),
+    Card('BBB', 3, 0),
+    Card('CCC', 4, 0),
+    Card('DDD', 5, 0),
+    Card('DDD', 6, 0),
+    Card('DDD', 7, 0),
 ]
 
 
 class Proto3Model(object):
-    def __init__(self, cursor, level):
-        self.cursor = cursor
-        self.level = level
+    def __init__(self):
         self.todo = queue.SimpleQueue()
         self.redo = queue.SimpleQueue()
-        self.card = None
+        self.done = queue.LifoQueue()
+
+        for card in CARD_DECK:
+            self.todo.put(card)
+
+        assert self.todo.qsize() > 0
+        self.card = self.todo.get()
+        self.section = self.card.section
+
+    def dump_state(self):
+        state = '''
+        ============
+          TODO: {todo}
+          REDO: {redo}
+          DONE: {done}
+          CURRENT: {section} / {card}
+       ============ 
+        '''
+        print(state.format(
+            todo=self.todo.qsize(), redo=self.redo.qsize(), done=self.done.qsize(),
+            section=self.section, card=self.card))
 
     def on_enter_BUILD(self):
-        print('building!')
+        print('building')
         self.homeview()
 
     def on_enter_HOME_VIEW(self):
         print('homeview...')
+        view = '''
+        +-------------
+        | HOME
+        |
+        | * study
+        | * settings
+        +-------------
+        '''
+        print(view)
+        self.dump_state()
 
     def on_enter_ANY_TO_STUDY(self):
         print('any to study?')
-        if 0:
-            self.any_to_study_no()
-        else:
+        print('  todo:', self.todo.qsize())
+        print('  card[%s]' %  self.card)
+        if self.todo.qsize() > 0 or self.card:
+            print('  yes')
             self.any_to_study_yes()
+        else:
+            print('  no')
+            self.any_to_study_no()
 
     def on_enter_DONE(self):
         print('done!')
 
     def on_enter_SECTION_VIEW(self):
-        print('Section view!')
+        view = '''
+        +--------
+        | SECTION
+        |
+        |    {section}
+        |
+        |  * front
+        |  * home
+        +----------
+        '''
+        print(view.format(section=self.card.section))
+        self.front()
 
     def on_enter_FRONT_VIEW(self):
-        print('Front view!')
+        view = '''
+         +--------
+         | FRONT
+         |
+         |    {card}
+         |
+         |  * flip
+         |  * home
+         +----------
+         '''
+        print(view.format(card=self.card))
+        self.flip()
 
     def on_enter_BACK_VIEW(self):
-        print('Back view!')
-
-    def on_enter_WRONG(self):
-        print('WRONG!')
+        view = '''
+         +--------
+         | BACK
+         |
+         |    {card}
+         |
+         |  * correct
+         |  * wrong
+         +----------
+         '''
+        print(view.format(card=self.card))
+        if random.random() < 0.33:
+            self.correct()
+        else:
+            self.wrong()
 
     def on_enter_CORRECT(self):
         print('CORRECT!')
+        self.card.update(3, TODAY)
+        self.done.put(self.card)
+        self.card = None
+        self.correct2()
+
+    def on_enter_WRONG(self):
+        print('WRONG!')
+        self.card.update(0, TODAY)
+        self.redo.put(self.card)
+        self.card = None
+        self.wrong2()
+
+    def on_enter_MORE_TODO(self):
+        print('More to do?')
+        self.dump_state()
+        if self.todo.qsize() > 0:
+            print('  yes')
+            self.card = self.todo.get()
+            self.more_todo_yes()
+        else:
+            print('  no')
+            self.more_todo_no()
+
+    def on_enter_ANY_TO_REDO(self):
+        assert self.todo.qsize() == 0
+        assert not self.card
+        print('Any to redo?')
+
+        if self.redo.qsize() > 0:
+            while self.redo.qsize() > 0:
+                card = self.redo.get()
+                self.todo.put(card)
+            self.card = self.todo.get()
+            self.any_to_redo_yes()
+        else:
+            self.any_to_redo_no()
+
+    def on_enter_NEW_SECTION(self):
+        print('new section?')
+        self.dump_state()
+        if self.section != self.card.section:
+            print('  yes')
+            self.new_section_yes()
+        else:
+            print('  no')
+            self.new_section_no()
 
 # The states
 class States(enum.Enum):
@@ -82,8 +218,6 @@ class States(enum.Enum):
     NEW_SECTION = 12
     DONE = 99
 
-# And some transitions between states. We're lazy, so we'll leave out
-# the inverse phase transitions (freezing, condensation, etc.).
 transitions = [
     ['build', States.START, States.BUILD ],
     ['homeview', States.BUILD, States.HOME_VIEW ],
@@ -99,22 +233,22 @@ transitions = [
     ['correct', States.BACK_VIEW, States.CORRECT],
     ['correct2', States.CORRECT, States.MORE_TODO],
     ['wrong2', States.WRONG, States.MORE_TODO],
-    ['more_to_do_yes', States.MORE_TODO, States.NEW_SECTION],
-    ['more_to_do_no', States.MORE_TODO, States.ANY_TO_REDO],
+    ['more_todo_yes', States.MORE_TODO, States.NEW_SECTION],
+    ['more_todo_no', States.MORE_TODO, States.ANY_TO_REDO],
     ['any_to_redo_yes', States.ANY_TO_REDO, States.NEW_SECTION],
     ['any_to_redo_no', States.ANY_TO_REDO, States.DONE],
     ['new_section_yes', States.NEW_SECTION, States.SECTION_VIEW],
     ['new_section_no', States.NEW_SECTION, States.FRONT_VIEW]
 ]
 
+def main():
+    model = Proto3Model()
+    machine = Machine(model, states=States, transitions=transitions, initial=States.START)
+    model.build()
+    model.study()
+    while(model.state != States.DONE):
+        model.front()
 
-model = Proto3Model('cursor', 'A1')
-machine = Machine(model, states=States, transitions=transitions, initial=States.START)
-print(model.state)
-model.build()
-model.study()
-model.front()
-model.flip()
-model.flip_back()
-model.flip()
-model.correct()
+
+if __name__ == '__main__':
+    main()
